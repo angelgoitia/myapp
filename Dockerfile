93e387f86a4ae47f1bb43371d82f83723f782ad4
angelgoitia <angelgoitia1995@gmail.com>
2019-12-30 10:41:49 -0400

    ajuste de docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM ruby:2.5
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev sqlite3 nodejs
+FROM ruby:2.5.1
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev sqlite3
+RUN curl -sL https://deb.nodesource.com/setup_10.x | bash - && apt-get install -y nodejs
 RUN mkdir /myapp
 WORKDIR /myapp
 COPY Gemfile /myapp/Gemfile
--- db/schema.rb
+++ db/schema.rb
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 2019_12_26_220532) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "cedula"
-    t.string "role"
+    t.string "name", null: false
+    t.string "cedula", null: false
+    t.string "role", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
